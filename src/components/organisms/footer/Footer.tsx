fae1f01a3fd9b364a3021f2472bbf2c6b97b8cdc
Adam Horodyski <ad.horodyski@gmail.com>
2021-10-11 15:44:05 +0200

    resolve conflicts

--- src/components/organisms/footer/Footer.tsx
+++ src/components/organisms/footer/Footer.tsx
@@ -1,23 +1,23 @@
-import Link from "next/link";
-
 export const Footer = () => (
     <footer className="w-full bg-white border-t-2 border-gray-100">
         <nav className="flex flex-col md:flex-row md:gap-32 container mx-auto px-3 gap-8 py-16">
-            <div className="text-gray-400">
-                <p>Maria Iwańska</p>
-                <p>ul. Dunajcowa 99</p>
-                <p>Nowy Sącz</p>
-                <p>NIP: 424242424</p>
+            <div className="flex flex-col gap-3">
+                <div>
+                    <span className="text-gray-400">Instagram: </span>
+                    <a
+                        href="https://www.instagram.com/color_care_"
+                        title="color_care_ na Instagramie"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        color_care_
+                    </a>
+                </div>
+                <div>
+                    <span className="text-gray-400">Email: </span>
+                    <a href="mailto:ad.horodyski@gmail.com">m.iwanska99@gmail.com</a>
+                </div>
             </div>
-            <a
-                href="https://www.instagram.com/color_care_"
-                title="color_care_ na Instagramie"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <span className="text-gray-400">Instagram:</span> color_care_
-            </a>
-            <Link href="/regulamin">Regulamin</Link>
         </nav>
     </footer>
 );
