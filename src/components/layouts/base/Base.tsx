54485734b6aa3fd350a02fc3ee919d29fe0ee629
Adam Horodyski <ad.horodyski@gmail.com>
2022-04-09 22:39:18 +0200

    fix: build errors

--- src/components/layouts/base/Base.tsx
+++ src/components/layouts/base/Base.tsx
@@ -1,10 +1,10 @@
-import { FC } from "react";
+import { FC, PropsWithChildren } from "react";
 import { Header, Footer } from "components/organisms";
 
-export const BaseLayout: FC = (p) => (
+export const BaseLayout: FC<PropsWithChildren<unknown>> = ({ children }) => (
     <>
         <Header />
-        <main className="container mx-auto px-3 py-6 mb-32">{p.children}</main>
+        <main className="container mx-auto px-3 py-6 mb-32">{children}</main>
         <Footer />
     </>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
     darkMode: "media",
     theme: {
         fontFamily: {
